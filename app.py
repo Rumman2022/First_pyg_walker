b80612fcb58ec4c5f3526afa853e93276c1005ba
Rumman2022 <111273185+Rumman2022@users.noreply.github.com>
2023-06-26 15:20:21 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+import pygwalker as pyg
+
+
+st.set_page_config(layout="wide")
+
+st.header("MY SECOND PROJECT ")
+
+
+
+
+
+def load_data(url):
+    df = pd.read_csv(url)
+
+    return df
+df = load_data(r"C:\Users\rumma\PycharmProjects\pythonProject15\bangalore.csv")
+
+
+
+def display(con):
+
+    with open(con,"r") as f:
+        file = f.read()
+    return file
+file = display(r"C:\Users\rumma\Desktop\config.json")
+pyg.walk(df,env="Streamlit",spec=file,dark="light")
+
